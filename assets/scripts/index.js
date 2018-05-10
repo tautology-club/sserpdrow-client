068c530f745e48626485ccd18c1e8186e9e272c9
sparrowr <sparrow@srubin.org>
2018-05-10 10:12:39 -0400

    updated UI for page and auth
    - tidied up user feedback on auth actions
    - began to construct page handlebars
    - built modal for page creation
    - began to construct page API JS
    
    paired with:
    * PatODay
    * Matty-Montoya

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,8 +6,10 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 const authEvents = require('./auth/events.js')
+const pageEvents = require('./page/events.js')
 
 $(() => {
   // AddHandlers
   authEvents.addHandlers()
+  pageEvents.addHandlers()
 })
