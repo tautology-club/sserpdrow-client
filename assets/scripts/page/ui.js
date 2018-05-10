7adbd9cd2ab068e6bbe9f2ac5fdc18bd881abdaf
PatODay <poday22@gmail.com>
2018-05-10 11:51:45 -0400

    Pages Create, set up Read, started update
    In index.html finished work with modals, added buttons for show all & show my pages.
    In page/ui.js added UI features for GET /pages requests via handlebars.
    In page/events.js added eventHandler for GET all and GET my pages
    In auth/ui.js removed class hidden for buttons for SHOW on sign-in
    In templates/my-page.handlebars added handlebars for showing all of a users pages.
    
    Paired with:
      * Mateo
      * Sparrow
      * Pat

--- assets/scripts/page/ui.js
+++ assets/scripts/page/ui.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const showPagesTemplate = require('../templates/page.handlebars')
+const showMyPagesTemplate = require('../templates/my-page.handlebars')
+const store = require('../store.js')
+
 const createPageSuccess = function () {
   $('.status').text('You have successfully created a page!')
   setTimeout(() => $('.status').text(''), 2000)
@@ -12,7 +16,34 @@ const createPageFailure = function () {
   setTimeout(() => $('.status').text(''), 2000)
 }
 
+const getPagesSuccess = function (data) {
+  console.log('getPagesSuccess data is', data)
+  const getPagesHTML = showPagesTemplate({pages: data.pages})
+  $('.content').html(getPagesHTML)
+  if (data.pages.length === 0) {
+    $('.content').html('<h2>No pages were found</h2>')
+  }
+}
+
+const getMyPagesSuccess = function (data) {
+  console.log('getMyPagesSuccess data is', data)
+  console.log('getMyPagesSuccess store is', store)
+  const myPages = []
+  data.pages.forEach(function (page) {
+    if (page.owner === store.user._id) {
+      myPages.push(page)
+    }
+  })
+  const getMyPagesHTML = showMyPagesTemplate({pages: myPages})
+  $('.content').html(getMyPagesHTML)
+  if (data.pages.length === 0) {
+    $('.content').html('<h2>No pages were found</h2>')
+  }
+}
+
 module.exports = {
   createPageSuccess,
-  createPageFailure
+  createPageFailure,
+  getPagesSuccess,
+  getMyPagesSuccess
 }
