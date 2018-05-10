2b3d593fb166cf024767c5604612cc762755c245
Matty-Montoya <mmontoya92@gmail.com>
2018-05-10 13:47:43 -0400

    Create update modal in my-page handlebars
    Update modal
    -Update the name and text/textarea of the page created.
    -Auto refresh the mypage content div.
    
    Add remove button;however, no functionality.
    
    Paired:
    *Sparrow
    *Pat
    *Matt

--- assets/scripts/page/api.js
+++ assets/scripts/page/api.js
@@ -27,7 +27,21 @@ const getPages = function () {
   })
 }
 
+const updatePages = function (data, pageId) {
+  console.log(data)
+  return $.ajax({
+    url: config.apiUrl + '/pages/' + pageId,
+    method: 'PATCH',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
 module.exports = {
   createPage,
-  getPages
+  getPages,
+  updatePages
 }
