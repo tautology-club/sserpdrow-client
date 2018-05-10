fc016aa8f16afac6ca8cd85c58300813d5952ac5
PatODay <poday22@gmail.com>
2018-05-10 10:51:56 -0400

    Create Page feature
    In index.html added Bootstrap modal for create page
    In api.js added AJAX request for createPage
    In events.js added eventHandler for createPage
    In ui.js added success and failure messaging for create page
    
    Paired with:
      * Mateo
      * Sparrow
      * Pat

--- assets/scripts/page/events.js
+++ assets/scripts/page/events.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const getFormFields = require('../../../lib/get-form-fields')
+const api = require('./api')
+const ui = require('./ui')
+
+const onCreatePage = (event) => {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.createPage(data)
+    .then(ui.createPageSuccess)
+    .catch(ui.createPageFailure)
+}
+
+const addHandlers = () => {
+  $('#create-page').on('submit', onCreatePage)
+}
+
+module.exports = {
+  addHandlers
+}
